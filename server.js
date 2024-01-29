0448cb94294610e6aa558b8a7b0689f3facf9d6e
aditya dand <33375699+adityadand@users.noreply.github.com>
2024-01-29 17:26:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const { spawn } = require('child_process');
 
 
 const app = express();
-const port = process.env.PORT || 5555;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
